16b4cf18d4deef982a5137bc100d0f4a750af5b8
CD821 <CD821@users.noreply.github.com>
2026-08-28 10:08:33 -0500

    Add August 27 dispatch jobs

--- app/data/job-seeds.ts
+++ app/data/job-seeds.ts
@@ -3,10 +3,12 @@ import baseJobs from "./jobs.json";
 import august24Jobs from "./jobs-2026-08-24.json";
 import august25Jobs from "./jobs-2026-08-25.json";
 import august26Jobs from "./jobs-2026-08-26.json";
+import august27Jobs from "./jobs-2026-08-27.json";
 
 export const sourceJobs = [
   ...(baseJobs as JobRecord[]),
   ...(august24Jobs as JobRecord[]),
   ...(august25Jobs as JobRecord[]),
   ...(august26Jobs as JobRecord[]),
+  ...(august27Jobs as JobRecord[]),
 ];
